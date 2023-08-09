818a6e037e9a7593d30f6900ca163803467e2eda
Leena200 <leenashahare90@gmail.com>
2023-08-09 20:20:14 +0530

    variable

---
+++ Variables.java
@@ -0,0 +1,10 @@
+class Variables{
+    public static void main(String[]arge){
+        int a = 20;
+        int b = 45;
+       // int sum = a + b;
+            //System.out.print(sum);
+            int diff = b - a;
+System.out.print(diff);
+    }
+}
