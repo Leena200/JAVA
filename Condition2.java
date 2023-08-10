12ad9804e22d5b9abe991a35c5b82417a5133b2e
Leena200 <leenashahare90@gmail.com>
2023-08-10 23:29:54 +0530

    loop statement

--- Condition2.java
+++ Condition2.java
@@ -2,6 +2,21 @@ import java.util.*;
 
 public class Condition2 {
     
-    public static void main(String args[]);
-    Scanner sc
+    public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+   int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    
+    
+    if(a == b){
+        System.out.println("equal");
+    }else{
+        if(a > b){
+            System.out.println("a is greater");
+        } else {
+            System.out.println("a is lesser");
+        }
+    }
+}
 }
---
+++ LoopStatements.java
@@ -0,0 +1,29 @@
+
+public class LoopStatements {
+    
+    public static void main(String agrs[]){
+        // for loop
+     //   for( int i=0; i < 11; i++){
+ // System.out.println(i);
+       // }
+      // other form
+
+     // for( int count = 0; count < 11; count++){
+    //    System.out.println(count);
+    //  }
+
+    // while loop
+   // int i = 0;
+   // while(i < 11){
+     //   System.out.println(i);
+     //   i = i + 1; 
+   // }
+
+   // do while loop
+   int i = 0;
+   do{
+    System.out.println(i);
+    i = i +1;
+   } while(i < 10);
+    }
+}
